33abc3195d08b33fc622415eaecc5c42d6cf4f5c
Vaishnavi Shinde <vaishnavishinde3107@gmail.com>
2024-01-06 20:28:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/signuppagewithdatabse/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.signuppagewithdatabse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var db: DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nameInput = findViewById<EditText>(R.id.name)
+        val usernameInput = findViewById<EditText>(R.id.username)
+        val emailInput = findViewById<EditText>(R.id.email)
+        val passInput = findViewById<EditText>(R.id.password)
+        val signUpbtn = findViewById<Button>(R.id.button)
+
+        signUpbtn.setOnClickListener {
+            val name = nameInput.text.toString()
+            val username = usernameInput.text.toString()
+            val email = emailInput.text.toString()
+            val password = passInput.text.toString()
+
+            val list = User(name, username, email,password)
+
+            db = FirebaseDatabase.getInstance().getReference("user")
+
+            db.child(username).setValue(list).addOnSuccessListener {
+                Toast.makeText(this, "User Registered", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "failed to register user", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/signuppagewithdatabse/User.kt
@@ -0,0 +1,3 @@
+package com.example.signuppagewithdatabse
+
+data class User(val name: String, val username: String, val email: String, val password: String)
